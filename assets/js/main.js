6641b0f6f7c99f1576209bddeac799af082a503f
Salano24 <salernomarco48@email.com>
2022-10-27 15:16:12 +0200

    fine esercizio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,41 @@
-const slides = [
-    {
-        image: './assets/img/01.webp',
-            title: 'Marvel\'s Spiderman Miles Morale',
-            text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-        }, {
-            image: './assets/img/02.webp',
-            title: 'Ratchet & Clank: Rift Apart',
-            text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-        }, {
-            image: './assets/img/03.webp',
-            title: 'Fortnite',
-            text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-        }, {
-            image: './assets/img/04.webp',
-            title: 'Stray',
-            text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-        }, {
-            image: './assets/img/05.webp',
-            title: "Marvel's Avengers",
-            text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-        }
-    ];
-
-const {
-    createApp 
-} = Vue
-
+const { createApp } = Vue
 createApp({
-    data(){
-        return{
-            
-        }
+  data() {
+    return {
+        seasons: [
+            {Image: "01.webp",
+            Title: "Spiderman"
+            }, 
+            {Image: "02.webp",
+            Title: "Ratchet And Clank"
+            }, 
+            {Image: "03.webp",
+            Title: "Fortnite" 
+            }, 
+            {Image: "04.webp",
+            Title: "Stray" 
+            },
+            {Image: "05.webp",
+            Title: "Avengers" 
+            }
+        ],
+        source: "./assets/img/",
+        counter: 0,
+    }
+  }, 
+  methods:{
+    avanti(){
+      this.counter++
+      if(this.counter >= this.seasons.length){
+        this.counter=0
+      }
     },
-    
+    dietro(){
+      this.counter--
+      if(this.counter < 0){
+        this.counter = this.seasons.length-1
+      }
+    },
+  }
+
 }).mount('#app')
